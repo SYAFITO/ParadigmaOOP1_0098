b2e11f78030732020a14aed3447fd2d3f0b446c3
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-20 14:36:31 +0700

    first commit

---
+++ Contoh1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+   mahasiswa(int pId,string pNama,float pNilai) :id(pId), nama(pNama), nilai (pNilai)
+   //Mem    
+}
