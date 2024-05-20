09fbb631568e1b4111c191b0cce1b6eec3d99587
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-20 15:28:32 +0700

    akhir dari header

--- Aggregasi/contoh.cpp
+++ Aggregasi/contoh.cpp
@@ -8,5 +8,16 @@ int main() {
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
-    
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak2();
+
+    delete varibu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
 }
