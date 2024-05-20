3052d08896bab4bdf9517d8e01f6e4ac4eb6ce48
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-20 14:39:48 +0700

    masukkan class mahasiswa

--- Contoh1.cpp
+++ Contoh1.cpp
@@ -8,5 +8,9 @@ class mahasiswa {
 
 public:
    mahasiswa(int pId,string pNama,float pNilai) :id(pId), nama(pNama), nilai (pNilai)
-   //Mem    
-}
+   { //Member Initialization List
+     //Definisi
+   }
+    
+      
+};
